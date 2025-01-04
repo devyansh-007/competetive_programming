bcc8557951576a1d482e1c92514e5e2deba42432
Devyansh Sharma <129656658+devyansh-007@users.noreply.github.com>
2025-01-04 12:10:30 +0530

    Update string_hashing.cpp

--- stering_hashing.cpp
+++ stering_hashing.cpp
@@ -1,8 +1,11 @@
-
-long long get_hash(string s){
-	long long h = 0;
-	for(char c : s){
-		h = (h*31 + (c-'a'+1))%MOD;
-	}
-	return h;
+long long compute_hash(string const& s) {
+    const int p = 31;
+    const int m = 1e9 + 9;
+    long long hash_value = 0;
+    long long p_pow = 1;
+    for (char c : s) {
+        hash_value = (hash_value + (c - 'a' + 1) * p_pow) % m;
+        p_pow = (p_pow * p) % m;
+    }
+    return hash_value;
 }
